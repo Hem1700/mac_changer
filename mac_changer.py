3852c5a93d06234a47311a3f3a12a30526f2991f
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-11 10:50:10 +0530

    Added get_arguments function

--- mac_changer.py
+++ mac_changer.py
@@ -3,21 +3,20 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m" , "--mac" , dest="new_mac" , help = "New MAC address")
+    return parser.parse_args()
+
+
 def change_mac(interface , new_mac):
     print("[+] Changing MAC Address for " + interface + " to" + new_mac)
-    # subprocess.call("ifconfig " + interface + " down ", shell=True)
-    # subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-    # subprocess.call("ifconfig " + interface + " up ", shell=True)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m" , "--mac" , dest="new_mac" , help = "New MAC address")
-
-(options, argument)= parser.parse_args()
+(options, arguments) = get_arguments()
 change_mac(options.interface , options.new_mac)
 
 
