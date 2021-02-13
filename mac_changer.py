dd956c3f70cc8aae046bbfdb2135ecd1fe90c0dc
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-13 15:15:04 +0530

    Regex implemented

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -21,9 +22,21 @@ def change_mac(interface , new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac():
+    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w" , ifconfig_result.decode("utf-8"))
+    if mac_address_search_result:
+       return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")    
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface , options.new_mac)
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
+
+
+
 
 
