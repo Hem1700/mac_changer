51fd13d591a86c2e1b912cd5fb95334e0ba6fe41
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-13 11:35:29 +0530

    Conditions added

--- mac_changer.py
+++ mac_changer.py
@@ -7,7 +7,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m" , "--mac" , dest="new_mac" , help = "New MAC address")
-    return parser.parse_args()
+    (options, arguments) =  parser.parse_args()
+    if not options.interface:
+        parser.error("[+] Plese specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[+] Plese specify an MAC address, use --help for more info")   
+    return options    
 
 
 def change_mac(interface , new_mac):
@@ -16,7 +21,7 @@ def change_mac(interface , new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface , options.new_mac)
 
 
